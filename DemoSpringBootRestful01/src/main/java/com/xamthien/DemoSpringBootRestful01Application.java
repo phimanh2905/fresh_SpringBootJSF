355213c74a974ac234c6a3d8437d43e573810c99
xamthien <cuochanhtrinhbattan@gmail.com>
2018-07-19 17:00:26 +0700

    basic restfull

---
+++ DemoSpringBootRestful01/src/main/java/com/xamthien/DemoSpringBootRestful01Application.java
@@ -0,0 +1,12 @@
+package com.xamthien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootRestful01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootRestful01Application.class, args);
+	}
+}
