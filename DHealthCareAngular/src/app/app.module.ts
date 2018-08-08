1d2de1a0f898ad9115116c2f6737e357873a1dc6
xamthien <cuochanhtrinhbattan@gmail.com>
2018-08-08 11:55:23 +0700

    error css

--- DHealthCareAngular/src/app/app.module.ts
+++ DHealthCareAngular/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app.routing.module';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { TrangchuComponent } from './pages/trangchu/trangchu.component';
@@ -23,7 +26,10 @@ import { ServicesComponent } from './pages/services/services.component';
     ServicesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
