031e2119fb6c488d9cf74cc7cc2acdd5fd719cb9
xamthien <cuochanhtrinhbattan@gmail.com>
2018-08-28 17:56:24 +0700

    js error

--- HealthCare_BT/DHealthCareAngular/src/app/app.component.ts
+++ HealthCare_BT/DHealthCareAngular/src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DHealthCareAngular';
+//    ngOnInit() {
+////      this.loadScript('http://www.some-library.com/library.js');
+//      this.loadScript('../assets/js/super_guacamole_min.js');
+//  }
+//  public loadScript(url: string) {
+//    const body = <HTMLDivElement> document.body;
+//    const script = document.createElement('script');
+//    script.innerHTML = '';
+//    script.src = url;
+//    script.async = false;
+//    script.defer = true;
+//    body.appendChild(script);
+//  }
 }
--- HealthCare_BT/DHealthCareAngular/src/app/pages/menu/menu.component.ts
+++ HealthCare_BT/DHealthCareAngular/src/app/pages/menu/menu.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Directive,Component, OnInit, Input } from '@angular/core';
 //import '../../../assets/js/super_guacamole_min.js';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css'],
 })
-export class MenuComponent implements OnInit {
 
-  constructor() { }
+export class MenuComponent implements OnInit {
+  loadAPI: Promise<any>;
+  constructor() {
+  this.loadAPI = new Promise((resolve) => {
+        this.loadScript();
+        resolve(true);
+    });
+}
 
   ngOnInit() {
-  }
 
+  }
+  public loadScript() {        
+    var isFound = false;
+    var scripts = document.getElementsByTagName("script")
+    for (var i = 0; i < scripts.length; ++i) {
+        if (scripts[i].getAttribute('src') != null && scripts[i].getAttribute('src').includes("loader")) {
+            isFound = true;
+        }
+    }
+
+    if (!isFound) {
+        var dynamicScripts = ["assets/js/super_guacamole_min.js"];
+
+        for (var i = 0; i < dynamicScripts .length; i++) {
+            let node = document.createElement('script');
+            node.src = dynamicScripts [i];
+            node.type = 'text/javascript';
+            node.async = false;
+//            node.charset = 'utf-8';
+            let body = <HTMLDivElement> document.body;
+            body.appendChild(node);
+        }
+
+    }
+}
 }
