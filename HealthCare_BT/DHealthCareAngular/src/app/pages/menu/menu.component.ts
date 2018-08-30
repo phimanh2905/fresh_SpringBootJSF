b80e3afc9232d632f6833ecd605a10d3cdd2b680
xamthien <cuochanhtrinhbattan@gmail.com>
2018-08-30 18:01:57 +0700

    fixed error js

--- HealthCare_BT/DHealthCareAngular/src/app/pages/menu/menu.component.ts
+++ HealthCare_BT/DHealthCareAngular/src/app/pages/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Directive,Component, OnInit, Input } from '@angular/core';
+import { Directive,Component, OnInit, Input, AfterViewInit } from '@angular/core';
 //import '../../../assets/js/super_guacamole_min.js';
 @Component({
   selector: 'app-menu',
@@ -6,11 +6,11 @@ import { Directive,Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./menu.component.css'],
 })
 
-export class MenuComponent implements OnInit {
+export class MenuComponent implements OnInit, AfterViewInit  {
   loadAPI: Promise<any>;
   constructor() {
   this.loadAPI = new Promise((resolve) => {
-        this.loadScript();
+//        this.loadScript();
         resolve(true);
     });
 }
@@ -18,7 +18,16 @@ export class MenuComponent implements OnInit {
   ngOnInit() {
 
   }
-  public loadScript() {        
+  
+  ngAfterViewInit() {
+    // ...
+    setTimeout(() => {
+      this.loadScript();
+    }, 1000);
+    
+  }
+  public loadScript() {  
+//    alert(1)      
     var isFound = false;
     var scripts = document.getElementsByTagName("script")
     for (var i = 0; i < scripts.length; ++i) {
@@ -27,8 +36,38 @@ export class MenuComponent implements OnInit {
         }
     }
 
-    if (!isFound) {
-        var dynamicScripts = ["assets/js/super_guacamole_min.js"];
+    if (/*!isFound*/ true ) {
+        var dynamicScripts = [
+          "assets/js/cherry-js-core.min.js",
+          "assets/js/spin.min.js",
+          "assets/js/spin.jquery.js",
+          "assets/js/jquery.tooltipster.min.js",
+          "assets/js/functions.js",
+          "assets/js/frontend-builder-global-functions.js",
+          "assets/js/scripts.js",
+          "assets/js/cherry-handler.min.js",
+          "assets/js/cherry-post-formats.min.js",
+          "assets/js/jquery.fitvids.js",
+          "assets/js/waypoints.min.js",
+          "assets/js/jquery.magnific-popup.min.js",
+          "assets/js/jquery.mobile.custom.min.js",
+          "assets/js/jquery.closest-descendent.js",
+          "assets/js/jquery.reverse.js",
+          "assets/js/jquery.tm-pb-simple-carousel.js",
+          "assets/js/jquery.tm-pb-simple-slider.js",
+          "assets/js/jquery.easypiechart.js",
+          "assets/js/tm-hash.js",
+          "assets/js/scripts(1).js",
+          "assets/js/jquery.fittext.js",
+          "assets/js/hoverIntent.min.js",
+          "assets/js/super_guacamole_min.js",
+          "assets/js/swiper(1).jquery.min.js",
+          "assets/js/jquery.ui.totop.min.js",
+          "assets/js/theme-script.js",
+          "assets/js/wp-embed.min.js"
+          
+          
+        ];
 
         for (var i = 0; i < dynamicScripts .length; i++) {
             let node = document.createElement('script');
@@ -41,5 +80,5 @@ export class MenuComponent implements OnInit {
         }
 
     }
-}
+  }
 }
--- HealthCare_BT/DHealthCareAngular/src/app/service/servicesname.service.ts
+++ HealthCare_BT/DHealthCareAngular/src/app/service/servicesname.service.ts
@@ -4,36 +4,36 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { SERVICE_ITEMS } from '../data/services.data';
 import { ServiceName } from '../models/servicesname.model';
 
-const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-};
+//const httpOptions = {
+//  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+//};
 
 @Injectable()
 export class ServicesNameService {
 //  private services = SERVICE_ITEMS;
-//  services: ServiceName[];
+  services: ServiceName[];
   constructor(private http: HttpClient) {}
   
-  private domain = 'http://192.188.88.119:8090';
+//  private domain = 'http://192.188.88.119:8090';
   //==============================================
   
-    public getAllServices() {
-      return this.http.get<ServiceName[]>(this.domain + '/api/services');
-  }
-  //==============================================
-  //  public getAllServices() {
-//    const sv1 = new ServiceName('fa fa-user-md', 'Prescriptions', 'Treating some psychological disorders and some psychological issues may require a combination of both therapy and some medication...', '#');
-//    const sv2 = new ServiceName('fa fa-stethoscope','Interventions','If you want to make a difference for one of your friend\'s or family member\'s life now, a professional intervention is the answer!', '#');
-//    const sv3 = new ServiceName('fa fa-home','Family Therapy','With physical activity being a huge part of one\'s health and wellbeing, it can help you lose weight too!','#');
-//    const sv4 = new ServiceName('fa fa-heart','Anxiety Therapy','With no one-fits-it-all diet program existing, we will custom-tailor a dieting program just for you!','#');
-//    const sv5 = new ServiceName('fa fa-umbrella','Depression Therapy','Behind any overweight issue, there\'s a health problem that\'s hiding. We want to diagnose and treat it!','#');
-//    const sv6 = new ServiceName('fa fa-user-plus','Personal Coaching','When it comes to weight loss treatments, children need an extra special attention given to them...','#');
-//    const sv7 = new ServiceName('fa fa-plus-circle','Child Therapy','Just as it goes for juniors, seniors need a special approach when it comes to weight loss solutions.','#');
-//    const sv8 = new ServiceName('fa fa-child','Psychotherapy','Bariatric (or stomach-related) surgeries are the last option for fixing excessive weight issue.','#');
-//
-//    this.services = [sv1, sv2, sv3, sv4, sv5, sv6, sv7, sv8];
-//    return this.services;
+//    public getAllServices() {
+//      return this.http.get<ServiceName[]>(this.domain + '/api/services');
 //  }
+  //==============================================
+    public getAllServices() {
+    const sv1 = new ServiceName('fa fa-user-md', 'Prescriptions', 'Treating some psychological disorders and some psychological issues may require a combination of both therapy and some medication...', '#');
+    const sv2 = new ServiceName('fa fa-stethoscope','Interventions','If you want to make a difference for one of your friend\'s or family member\'s life now, a professional intervention is the answer!', '#');
+    const sv3 = new ServiceName('fa fa-home','Family Therapy','With physical activity being a huge part of one\'s health and wellbeing, it can help you lose weight too!','#');
+    const sv4 = new ServiceName('fa fa-heart','Anxiety Therapy','With no one-fits-it-all diet program existing, we will custom-tailor a dieting program just for you!','#');
+    const sv5 = new ServiceName('fa fa-umbrella','Depression Therapy','Behind any overweight issue, there\'s a health problem that\'s hiding. We want to diagnose and treat it!','#');
+    const sv6 = new ServiceName('fa fa-user-plus','Personal Coaching','When it comes to weight loss treatments, children need an extra special attention given to them...','#');
+    const sv7 = new ServiceName('fa fa-plus-circle','Child Therapy','Just as it goes for juniors, seniors need a special approach when it comes to weight loss solutions.','#');
+    const sv8 = new ServiceName('fa fa-child','Psychotherapy','Bariatric (or stomach-related) surgeries are the last option for fixing excessive weight issue.','#');
+
+    this.services = [sv1, sv2, sv3, sv4, sv5, sv6, sv7, sv8];
+    return this.services;
+  }
  }
 
 
